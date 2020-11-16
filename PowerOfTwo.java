e5830e9d54fa8bb0d99ac58209984d8df74a2673
tanvir-ux <tanviralamshuvo@gmail.com>
2020-11-16 14:14:45 +0600

    power of two

---
+++ PowerOfTwo.java
@@ -0,0 +1,12 @@
+public class PowerOfTwo {
+    public static void main(String[] args){
+        int n = Integer.parseInt(args[0]);
+        int i = 0;
+        int v = 1;
+        while(i <= n){
+            System.out.println(v);
+            i = i + 1;
+            v = v * 2;
+        }
+    }
+}
