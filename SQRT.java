aab05655b961e90ab8d1663f6c0bb7ae7b00d199
tanvir-ux <tanviralamshuvo@gmail.com>
2020-11-16 15:41:57 +0600

    square root of only positive numbers

--- SQRT.java
+++ SQRT.java
@@ -1,7 +1,7 @@
 public class SQRT {
     public static void main(String[] args){
         double error = 1E-15;
-        double c = Double.parseDouble(args[0]);
+        double c = Math.abs(Double.parseDouble(args[0]));
         double t = c;
         while(Math.abs(t - c/t) > t*error){
             t = (c/t + t) / 2;
