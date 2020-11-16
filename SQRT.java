e93b49e89ebccaef8bc58cb596e2e8d91f49854d
tanvir-ux <tanviralamshuvo@gmail.com>
2020-11-16 15:28:27 +0600

    square root of n

---
+++ SQRT.java
@@ -0,0 +1,11 @@
+public class SQRT {
+    public static void main(String[] args){
+        double error = 1E-15;
+        double c = Double.parseDouble(args[0]);
+        double t = c;
+        while(Math.abs(t - c/t) > t*error){
+            t = (c/t + t) / 2;
+        }
+        System.out.println(t);
+    }
+}
